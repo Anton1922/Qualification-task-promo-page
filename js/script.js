e65ba29bd70b6dadbadc16de82e0cbd29896b1ce
Anton Fedorov <anton.pzkt@gmail.com>
2019-03-26 00:31:41 +0100

    added funtions for navigation

--- js/script.js
+++ js/script.js
@@ -1,24 +1,16 @@
 $(document).ready(function(){
-  $('.carousel').slick({
-    arrows: false,
-    vertical: true,
-    infinite: false,
-    verticalSwiping: true,
-  });
 
-  $('.tooltip').tooltipster({
-   	theme: 'tooltipster-light',
-    trigger: 'click'
+  $('form__button').click(function() {
+    $('.form__text, .form__link').show();
   });
 
 });
 
 $(document).ready(function(){
 
-  $('.form__text, .form__link').hide();
-
-  $('form__button').click(function() {
-    $('.form__text, .form__link').show();
+  $('.navigation__link').click(function() {
+    $('.navigation__link').removeClass('active');
+    $(this).addClass('active');
   });
 
 });
